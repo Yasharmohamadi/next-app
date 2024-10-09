4fc32c1993fb759982c3a42635a7080de7b5e41c
Yashar <yashar22mo@gmail.com>
2024-10-09 22:55:01 +0330

    add getStaticProps function

--- pages/users/index.js
+++ pages/users/index.js
@@ -17,8 +17,10 @@ export default function index(props) {
 	);
 }
 
-// this function handling server side rendering
-export async function getServerSideProps() {
+// PRE RENDERING
+// SSR = server side rendering : getServerSideProps() : send a html page for any user request.
+// SSG = static side generation : getStaticProps() :  save html code in a json file when site loaded.
+export async function getStaticProps() {
 	// here is server side
 	let res = await fetch("https://jsonplaceholder.typicode.com/users");
 	let users = await res.json();
