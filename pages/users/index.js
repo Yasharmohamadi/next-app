f273398b20c6bb22dda931d8b5cf62007735fe6d
Yashar <yashar22mo@gmail.com>
2024-10-09 21:55:12 +0330

    add getServerSideProps function

--- pages/users/index.js
+++ pages/users/index.js
@@ -2,22 +2,27 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-export default function index() {
-	const [users, setUsers] = useState([]);
-
-	useEffect(() => {
-		fetch("https://jsonplaceholder.typicode.com/users")
-			.then((res) => res.json())
-			.then((data) => setUsers(data));
-	}, []);
+export default function index(props) {
 
 	return (
 		<ul>
-			{users.map((user) => (
+			{props.users.map((user) => (
 				<li key={user.id}>
-					<Link href='/users/[username]' as={`/users/username`}>{user.name}</Link>
+					<Link href="/users/[username]" as={`/users/username`}>
+						{user.name}
+					</Link>
 				</li>
 			))}
 		</ul>
 	);
+}
+
+// this function handling server side rendering
+export async function getServerSideProps() {
+	// here is server side
+	let res = await fetch("https://jsonplaceholder.typicode.com/users");
+	let users = await res.json();
+	return {
+		props: { users },
+	};
 }
